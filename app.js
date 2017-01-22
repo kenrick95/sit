7142155bb38eb5ad9f649b765a5156f771e30964
kenrick95 <kenrick95@gmail.com>
2017-01-22 23:28:36 +0700

    addressing #6 with promise incomplete implementation 😴😴😴😴😴

--- app.js
+++ app.js
@@ -28,13 +28,68 @@ if (!String.prototype.startsWith) {
 //     determine what is a trending topic on Wikipedia
 
 const MILLISECONDS_IN_DAY = 86400000
-const NUMBER_OF_DAYS = 5
+const NUMBER_OF_DAYS = 14
 
 app.set('view engine', 'pug')
 
 app.use('/sit/static/chart.js', express.static('node_modules/chart.js/dist'))
 app.use('/sit/static/randomcolor', express.static('node_modules/randomcolor'))
 app.use('/sit/static', express.static('views/js'))
+
+var processDay = async(function (project, year, month, date) {
+  var response = await(baseRequest.getAsync('https://wikimedia.org/api/rest_v1/metrics/pageviews/top/' + project + '/all-access/'+ year +'/'+ month + '/' + date))
+  if (response.statusCode == 200) {
+    response = JSON.parse(response.body)
+    items = response.items
+    var resultDate = items[0].year + "-" + items[0].month + "-" + items[0].day
+
+    console.log(resultDate)
+
+    // TODO how to pass "excludeNamespaces"
+    // TODO how to pass "mainpage"
+    // TODO how to handle "articleCountByDay"
+
+    articles = items[0].articles
+    articles.forEach(function (v) {
+      // Filter: ignore non-article pages
+      for (var j = 0; j < excludeNamespaces.length; j++) {
+        if (v.article.startsWith(excludeNamespaces[j])) {
+          return
+        }
+      }
+
+      // Filter: ignore main page
+      if (v.article.startsWith(mainpage)) {
+        return
+      }
+
+      // for newly trending article
+      // pad zeros on left
+      if (!(v.article in articleCountByDay)) {
+        articleCountByDay[v.article] = []
+        for (var k = 0; k < i - 1; k++) {
+          articleCountByDay[v.article].push(0)
+        }
+      }
+
+      articleCountByDay[v.article].push(v.views)
+    })
+
+    // now I require all articleCountByDay items to be array of length "i",
+    // if not, pad right with zero
+    for (var key in articleCountByDay) {
+      if (articleCountByDay.hasOwnProperty(key)) {
+        var item = articleCountByDay[key]
+        if (item.length < i) {
+          var l = i - item.length
+          for (var k = 0; k < l; k++) {
+            articleCountByDay[key].push(0)
+          }
+        }
+      }
+    }
+  }
+})
 
 app.get('/sit/:project/until/:endTime', async(function (req, res) {
   var endTime = (new Date(req.params.endTime)).getTime()
@@ -55,63 +110,23 @@ app.get('/sit/:project/until/:endTime', async(function (req, res) {
   }
   var mainpage = siteinfo.query.general.mainpage.replace(' ', '_')
 
+  var promises = []
+
   for(var i = 1; i < NUMBER_OF_DAYS + 1; i++) {
     var loopTime = endTime - MILLISECONDS_IN_DAY * (NUMBER_OF_DAYS - i)
     var currentDate = new Date(loopTime)
     var year = currentDate.getFullYear()
     var month = pad(currentDate.getMonth() + 1, 2, '0')
     var date = pad(currentDate.getDate(), 2, '0')
-    var response = await(baseRequest.getAsync('https://wikimedia.org/api/rest_v1/metrics/pageviews/top/' + project + '/all-access/'+ year +'/'+ month + '/' + date))
-    if (response.statusCode == 200) {
-      response = JSON.parse(response.body)
-      items = response.items
-      var resultDate = items[0].year + "-" + items[0].month + "-" + items[0].day
+    var formattedDate = year + "-" + month + "-" + date
 
-      console.log(resultDate)
-      resultDates.push(resultDate)
-
-      articles = items[0].articles
-      articles.forEach(function (v) {
-        // Filter: ignore non-article pages
-        for (var j = 0; j < excludeNamespaces.length; j++) {
-          if (v.article.startsWith(excludeNamespaces[j])) {
-            return
-          }
-        }
-
-        // Filter: ignore main page
-        if (v.article.startsWith(mainpage)) {
-          return
-        }
-
-        // for newly trending article
-        // pad zeros on left
-        if (!(v.article in articleCountByDay)) {
-          articleCountByDay[v.article] = []
-          for (var k = 0; k < i - 1; k++) {
-            articleCountByDay[v.article].push(0)
-          }
-        }
-
-        articleCountByDay[v.article].push(v.views)
-      })
-
-      // now I require all articleCountByDay items to be array of length "i",
-      // if not, pad right with zero
-      for (var key in articleCountByDay) {
-        if (articleCountByDay.hasOwnProperty(key)) {
-          var item = articleCountByDay[key]
-          if (item.length < i) {
-            var l = i - item.length
-            for (var k = 0; k < l; k++) {
-              articleCountByDay[key].push(0)
-            }
-          }
-        }
-      }
-    }
+    // create promise
+    promises.push(new Promise(async(function (resolve, reject) {
+      return await(processDay(project, year, month, date))
+    })))
 
   }
+  await(promises)
 
   // Filtering
   // Arbitary number killing
