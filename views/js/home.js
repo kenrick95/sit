edc0b9cb8c9a5ea9c18f3b9313f8d582e91aa4a8
kenrick95 <kenrick95@gmail.com>
2017-01-25 22:10:47 +0700

    fix wrong address when home address isn't ending with '/'

--- views/js/home.js
+++ views/js/home.js
@@ -4,6 +4,9 @@ $(function () {
   $('#submit').click(function () {
     var project = $('#project').val()
     var endDate = $('#endDate').val()
+    if (window.location.href.lastIndexOf('/') < window.location.href.length - 1) {
+      project = '/' + project
+    }
     window.location.href = window.location.href + project + '/' + 'until' + '/' + endDate
   })
   $('#endDate').datepicker({
